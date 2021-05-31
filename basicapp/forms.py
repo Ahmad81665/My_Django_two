b7802cd382d9657bda490044a2450d37e1be277d
Ahmad Barho <ahmadbrhoo1993@gmail.com>
2021-05-31 12:59:42 +0200

    bots

--- basicapp/forms.py
+++ basicapp/forms.py
@@ -1,6 +1,18 @@
 from django import forms
-
+from django.core import validators
 class FormName(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
+    #crf hidden for security ex to ensure input dont going to another website
+    botcatcher = forms.CharField(required=False,
+                                 widget=forms.HiddenInput,
+                                 validators=[validators.MaxLengthValidator(0)])
+"""
+#we can do it loke validator= like up in botcatcher
+    def clean_botcatcher(self):
+        botcatcher = self.cleaned_data['botcatcher']
+        if len(botcatcher) > 0:
+            raise forms.ValidationError('GOTCHA BOT!')
+        return botcatcher
+"""
